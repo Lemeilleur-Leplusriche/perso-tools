015d1751d8c86bccd043ea08a38cb1268358510a
Lemeilleur-Leplusriche <geonhee.chae@gmail.com>
2026-03-09 14:46:29 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,5 +3,6 @@ self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
   if(e.request.url.includes('/hub/'))return;
+  if(e.request.url.includes('/tester/'))return;
   e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))
 });
