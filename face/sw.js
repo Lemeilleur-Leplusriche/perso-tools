0b0a36c1d2b2ca3ead74302cdc04201aeee3eaa7
Lemeilleur-Leplusriche <geonhee.chae@gmail.com>
2026-03-10 01:00:42 +0100

    Create sw.js

---
+++ face/sw.js
@@ -0,0 +1,8 @@
+const C='facesculpt-v1',A=['/perso-tools/face/','/perso-tools/face/index.html'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{
+  if(e.request.url.includes('fonts.googleapis.com'))return;
+  if(e.request.url.includes('fonts.gstatic.com'))return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(C).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))
+});
