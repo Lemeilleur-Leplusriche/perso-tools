dc40639dc3db336ad62f9a6698696a7d94a30593
Lemeilleur-Leplusriche <geonhee.chae@gmail.com>
2026-03-12 12:57:25 +0100

    Update sw.js

--- Alimentation/sw.js
+++ Alimentation/sw.js
@@ -1,4 +1,4 @@
-const C='recomp-v1',A=['/perso-tools/recomp/','/perso-tools/recomp/index.html'];
+const C='recomp-v1',A=['/perso-tools/Alimentation/','/perso-tools/Alimentation/index.html'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
